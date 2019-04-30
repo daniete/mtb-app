a864cf6ed869c7af8790a0b8a1007d6c5d487625
daniete <teun.daniels@geosolutions.be>
2019-04-30 13:12:43 +0200

    drag and drop gpx

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -5,10 +5,15 @@ import OlTileLayer from 'ol/layer/Tile';
 import OlView from 'ol/View';
 import Map from 'ol/Map.js';
 import View from 'ol/View.js';
-import Style from 'ol/style/Style.js';
 import Icon from 'ol/style/Icon.js';
 import {fromLonLat} from 'ol/proj.js';
+import {GPX, GeoJSON, IGC, KML, TopoJSON} from 'ol/format.js';
+import VectorLayer from 'ol/layer/Vector';
+import { defaults as defaultInteractions, DragAndDrop} from 'ol/interaction.js';
 import {defaults as defaultControls, FullScreen, ScaleLine} from 'ol/control.js';
+import { Vector as VectorSource} from 'ol/source.js';
+
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
 
 import Geocoder from 'ol-geocoder';
 
@@ -44,7 +49,18 @@ export class MapComponent implements OnInit {
       zoom: 8
     });
 
-    this.map = new Map({
+    var dragAndDropInteraction = new DragAndDrop({
+      formatConstructors: [
+        GPX,
+        GeoJSON,
+        IGC,
+        KML,
+        TopoJSON
+      ]
+    });
+
+    let map = new Map({
+      interactions: defaultInteractions().extend([dragAndDropInteraction]),
       layers: [this.layer],
       target: 'map',
       view: this.view,
@@ -72,7 +88,79 @@ export class MapComponent implements OnInit {
         })
       })
     });
-    this.map.addControl(this.geocoder);
+    map.addControl(this.geocoder);
+
+    var style = {
+      'Point': new Style({
+        image: new CircleStyle({
+          fill: new Fill({
+            color: 'rgba(255,255,0,0.4)'
+          }),
+          radius: 5,
+          stroke: new Stroke({
+            color: '#ff0',
+            width: 1
+          })
+        })
+      }),
+      'LineString': new Style({
+        stroke: new Stroke({
+          color: '#f00',
+          width: 3
+        })
+      }),
+      'MultiLineString': new Style({
+        stroke: new Stroke({
+          color: '#f00',
+          width: 3
+        })
+      })
+    };
+
+    dragAndDropInteraction.on('addfeatures', function(event) {
+      var vectorSource = new VectorSource({
+        features: event.features,
+        format: new GPX()
+      });
+      map.addLayer(new VectorLayer({
+        renderMode: 'image',
+        source: vectorSource,
+        style: function(feature) {
+          return style[feature.getGeometry().getType()];
+        }
+      }));
+      map.getView().fit(vectorSource.getExtent());
+    });
+
+    var displayFeatureInfo = function(pixel) {
+      var features = [];
+      map.forEachFeatureAtPixel(pixel, function(feature) {
+        features.push(feature);
+      });
+      if (features.length > 0) {
+        var info = [];
+        var i, ii;
+        for (i = 0, ii = features.length; i < ii; ++i) {
+          info.push(features[i].get('name'));
+        }
+        //document.getElementById('info').innerHTML = info.join(', ') || '&nbsp';
+      } else {
+        //document.getElementById('info').innerHTML = '&nbsp;';
+      }
+    };
+
+    map.on('pointermove', function(evt) {
+      if (evt.dragging) {
+        return;
+      }
+      var pixel = map.getEventPixel(evt.originalEvent);
+      displayFeatureInfo(pixel);
+    });
+
+    map.on('click', function(evt) {
+      displayFeatureInfo(evt.pixel);
+    });
 
   }
+
 }
