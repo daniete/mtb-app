51d8b2c2ae7aa72268b08e63ec808e8cfcb43619
daniete <teun.daniels@geosolutions.be>
2019-07-21 20:04:52 +0200

    team component init

---
+++ src/app/domain/team-config.ts
@@ -0,0 +1,31 @@
+export class TeamConfig {
+  name: string;
+  text: string;
+  imgSrc: string;
+}
+
+export const TEAMCONFIG: TeamConfig[] = [{
+  name: 'Jef Reyniers',
+  text: 'Woordje over Jef',
+  imgSrc: '../assets/img/jef.jpg'
+}, {
+  name: 'Lander van Tricht',
+  text: 'Woordje over Lander',
+  imgSrc: '../assets/img/lander.jpg'
+}, {
+  name: 'Thomas Sauvillers',
+  text: 'Woordje over Thomas',
+  imgSrc: '../assets/img/thomas.jpg'
+}, {
+  name: 'Vincent Lenaerts',
+  text: 'Woordje over Vincent',
+  imgSrc: '../assets/img/vincent.jpg'
+}, {
+  name: 'Maxim Panis',
+  text: 'Woordje over Maxim',
+  imgSrc: '../assets/img/maxim.jpg'
+}, {
+  name: 'Teun Daniels',
+  text: 'Woordje over Teun',
+  imgSrc: '../assets/img/teun.jpg'
+}];
